13abb486ffb9620bf38f8fff31cdee06246a3599
Gazzmatazz <rudrowe@yahoo.com>
2012-11-22 16:34:58 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FestApp::Application.config.secret_token = 'e191b2140c11e0b08a691481b90c0995c5e14ba548374790a4aabb3314e8bb2d6415603a56467fef69994cefd3d3608a5a12da8ed13b2c0762e5516961a412a8'
