57c8cb7c8f2c2b6400a6d3420590a4130078dd3c
Gazzmatazz <rudrowe@yahoo.com>
2012-11-25 17:44:57 +0100

    Update

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,9 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Fest Mate')
     end
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Fest Mate | Home")
+    it "should not have a custom page title" do
+        visit '/static_pages/home'
+        page.should_not have_selector('title', :text => '| Home')
     end
   end
 
